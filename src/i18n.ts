18c0c0f27b25cf1f33a23d5a490e709276d99fce
Aaqib Mir <aaqibmir.AB@gmail.com>
2026-01-17 14:26:23 +0000

    Try fixing this error: `Runtime TypeError: (0 , {imported module [projec

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,15 @@
 import {notFound} from 'next/navigation';
-import {getRequestConfig} from 'next-intl/server';
+import {getLocale, getRequestConfig} from 'next-intl/server';
  
 // Can be imported from a shared config
 const locales = ['en', 'ar'];
  
-export default getRequestConfig(async ({locale}) => {
+export default getRequestConfig(async () => {
+  // Obtain the locale
+  const locale = await getLocale();
+ 
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
+  if (!locales.includes(locale)) notFound();
  
   return {
     messages: (await import(`../messages/${locale}.json`)).default
