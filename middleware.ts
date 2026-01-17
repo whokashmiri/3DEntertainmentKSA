14745cedd28901919f0a8a115af8d38825131eb4
Aaqib Mir <aaqibmir.AB@gmail.com>
2026-01-17 12:50:58 +0000

    i am still seeing page not found

--- middleware.ts
+++ middleware.ts
@@ -9,8 +9,6 @@ export default createMiddleware({
 });
  
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+  // Match only internationalized pathnames
+  matcher: ['/', '/(ar|en)/:path*']
 };
