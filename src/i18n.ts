a09e44d90f1e246b0dc400b32ae0a2ad6ba052a9
Aaqib Mir <aaqibmir.AB@gmail.com>
2026-01-18 10:31:04 +0000

    ## Error Type Console Error
    ## Error Message
    
    A `locale` is expected to

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,17 @@
 import {notFound} from 'next/navigation';
-import {getRequestConfig} from 'next-intl/server';
+import {getRequestConfig, getLocale} from 'next-intl/server';
  
 // Can be imported from a shared config
 const locales = ['en', 'ar'];
  
-export default getRequestConfig(async ({locale}) => {
+export default getRequestConfig(async () => {
+  const locale = await getLocale();
+ 
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale)) notFound();
  
   return {
+    locale,
     messages: (await import(`../messages/${locale}.json`)).default
   };
 });
