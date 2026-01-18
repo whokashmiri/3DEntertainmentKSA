8309426bcb3aa723c6c773014724e9c385bb247b
Aaqib Mir <aaqibmir.AB@gmail.com>
2026-01-18 09:18:42 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,10 @@
-import {getLocale, getRequestConfig} from 'next-intl/server';
 import {notFound} from 'next/navigation';
+import {getRequestConfig} from 'next-intl/server';
  
 // Can be imported from a shared config
 const locales = ['en', 'ar'];
  
-export default getRequestConfig(async () => {
-  // Obtain the locale for the current request
-  const locale = await getLocale();
- 
+export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale)) notFound();
  
